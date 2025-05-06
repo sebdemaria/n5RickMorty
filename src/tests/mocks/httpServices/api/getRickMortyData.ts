58d1b307e4579eb3c8a53cf0f7da2af5e8b1dcee
Sebastián Demaría <53883705+sebdemaria@users.noreply.github.com>
2025-05-05 22:40:25 -0300

    FEATURE(#002): prettier

--- src/tests/mocks/httpServices/api/getRickMortyData.ts
+++ src/tests/mocks/httpServices/api/getRickMortyData.ts
@@ -1,9 +1,9 @@
 export const getRickMortyData = jest.fn(() =>
     Promise.resolve({
-      info: { count: 2, pages: 1, next: null, prev: null },
-      results: [
-        { id: 1, name: "Rick Sanchez", image: "rick.png" },
-        { id: 2, name: "Morty Smith", image: "morty.png" },
-      ],
+        info: { count: 2, pages: 1, next: null, prev: null },
+        results: [
+            { id: 1, name: "Rick Sanchez", image: "rick.png" },
+            { id: 2, name: "Morty Smith", image: "morty.png" }
+        ]
     })
-  );
+);
