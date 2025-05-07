4a53480d0440e5a724ff06743563ad5321a6564d
Sebastián Demaría <53883705+sebdemaria@users.noreply.github.com>
2025-05-07 13:36:27 -0300

    FEATURE(#004): install v8 coverage and test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,33 @@ export default defineConfig({
         globals: true,
         include: ["src/tests/**/*.{test,spec}.{ts,tsx}"],
         setupFiles: "./vitest.setup.ts",
-        css: true
+        css: true,
+        coverage: {
+            provider: "v8",
+            exclude: [
+                "src/App.tsx",
+                "src/main.tsx",
+                "src/theme.ts",
+                "src/i18n.ts",
+                "src/consts/**",
+                "src/tests/**",
+                "src/types/**",
+                "vitest.config.*",
+                "vite.config.*",
+                "jest.config.*",
+                "tailwind.config.*",
+                "postcss.config.*",
+                "tsconfig.*",
+                "*.config.*",
+                "**/config/**",
+                "**/*.d.ts",
+                "dist/assets/**",
+                "dist/assets/__federation_*.js",
+                "dist/assets/index-*.js",
+                "dist/assets/remoteEntry.js",
+                "dist/assets/_commonjsHelpers-*.js"
+            ]
+        }
     },
     resolve: {
         alias: {
